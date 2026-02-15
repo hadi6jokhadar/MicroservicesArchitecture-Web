c83d40db8da5a65ad5cc52658489fe16d073c60b
Hady Joukhadar <hadi6jokhadar@hotmail.com>
2026-02-15 13:11:44 +0300

    feat: Implement tenant list feature in the admin application, including UI, filtering, table display, and a new tenant interceptor.

--- libs/core/src/lib/interceptors/tenant.interceptor.ts
+++ libs/core/src/lib/interceptors/tenant.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tenantInterceptor: HttpInterceptorFn = (req, next) => {
-  const tenantId = localStorage.getItem('tenantId') ?? 'ihsandev';
+  const tenantId = localStorage.getItem('tenantId');
   if (tenantId) {
     req = req.clone({
       setHeaders: {
